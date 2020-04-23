9fa9953fe69570ece7211dfe1f4202f7e3205cd2
merle théo <merle.theo83@gmail.com>
2020-04-23 15:22:43 +0200

    commit
    mouvement + jump

---
+++ ForkProjetFinAnne/Assets/Scenes/script/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    //Movement
+    public float speed;
+
+    float moveVelocity;
+
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+
+    public bool isGrounded;
+    Rigidbody rb;
+    //Grounded Vars
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+    void OnCollisionExit()
+    {
+        isGrounded = false;
+    }
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && rb.velocity.y <= 0)
+        {
+
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+       
+
+            moveVelocity = 0;
+
+        //Left Right Movement
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.Q))
+        {
+            moveVelocity = -speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            moveVelocity = speed;
+        }
+
+        GetComponent<Rigidbody>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody>().velocity.y);
+
+
+    }
+    
+    
+    }
+
+        
+    
+
+    //Check if Grounded
