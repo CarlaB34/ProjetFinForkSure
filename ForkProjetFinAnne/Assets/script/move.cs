606d1ff0c4e4451ed36a5419375260e6b2f087a5
carla <carlita.b34@gmail.com>
2020-04-23 16:54:57 +0200

    test movePente, new folder, new scene + modé3d

---
+++ ForkProjetFinAnne/Assets/script/move.cs
@@ -0,0 +1,79 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class move : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+    Rigidbody rgb;
+    public bool isGrounded;
+
+    //public Transform feetPos;
+    //public float checkRadius;
+    //public LayerMask whatIsGround;
+
+    //private float jumpTimeCounter;
+    //public float jumpTime;
+    //private bool isJumping;
+
+    private void Start()
+    {
+        rgb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    //void OnCollisionStay()
+    //{
+    //    isGrounded = true;
+    //}
+    private void Update()
+    {
+        //right
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        }
+
+        //left
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.position += Vector3.right * -moveSpeed * Time.deltaTime;
+        }
+
+    //    isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+    //    //jump
+    //    if (Input.GetKey(KeyCode.Space) && isGrounded)
+    //    {
+    //        rgb.AddForce(jump * jumpForce, ForceMode.Impulse);
+    //        isGrounded = false;
+    //    }
+
+    //    if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //        rgb.AddForce(jump * jumpForce, ForceMode.Impulse);
+    //    }
+
+    //    if (Input.GetKey(KeyCode.Space) && isJumping == true)
+    //    {
+    //        if (jumpTimeCounter > 0)
+    //        {
+    //            rgb.AddForce(jump * jumpForce, ForceMode.Impulse);
+    //            jumpTimeCounter -= Time.deltaTime;
+    //            isGrounded = false;
+    //        }
+    //        else
+    //        {
+    //            isJumping = false;
+    //        }
+
+    //    }
+
+    //    if(Input.GetKeyUp(KeyCode.Space))
+    //    {
+    //        isJumping = false;
+    //    }
+    }
+}*/
---
+++ ForkProjetFinAnne/Assets/script/scriptMoveTheo.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptMoveTheo : MonoBehaviour
+{
+    CharacterController characterController;
+
+    //Movement
+    public float speed;
+
+    float moveVelocity;
+
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    public bool isGrounded;
+    Rigidbody rb;
+    //Grounded Vars
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        characterController = GetComponent<CharacterController>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+    void OnCollisionExit()
+    {
+        isGrounded = false;
+    }
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            // We are grounded, so recalculate
+            // move direction directly from axes
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.Q))
+            {
+                moveVelocity = -speed;
+            }
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            {
+                moveVelocity = speed;
+            }
+            //Left Right Movement
+
+            GetComponent<Rigidbody>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody>().velocity.y);
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        moveVelocity = 0;
+
+        
+
+
+    }
+
+
+}
