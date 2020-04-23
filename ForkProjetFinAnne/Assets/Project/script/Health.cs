8edc6d576c8aed2c2604bba56ff0d5c046c8febc
merle théo <merle.theo83@gmail.com>
2020-04-23 15:22:43 +0200

    commit
    mouvement + jump

---
+++ ForkProjetFinAnne/Assets/Project/script/Health.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int playerhealth = 2;
+
+    public void Update() // la vie est cap a 2hp
+    {
+        if(playerhealth > 2)
+    {
+            playerhealth = 2;
+        }
+    }
+    void OnCollisionEnter(Collision collision) // on perd un point de vie si on touche un pic
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("pike"))
+        {
+            Debug.Log("vous prenez 1 degat");
+            playerhealth -= 1;
+            
+        }
+        if (collision.gameObject.layer == LayerMask.NameToLayer("heal")) // on gagne un point de vie quand on rammasse un heal
+        {
+            Debug.Log("vous avez recupéré un point de vie");
+            playerhealth += 1;
+        }
+        if (playerhealth == 0) // si vie = 0 on meurt
+        {
+            Debug.Log("vous etes mort");
+            Destroy(gameObject);
+            
+        }
+    }
+}
---
+++ ForkProjetFinAnne/Assets/Project/script/Key.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{//la clé se detruit quand on passe dessus 
+   
+    void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player") )
+        {
+            Destroy(gameObject);
+        }
+     
+    }
+}
