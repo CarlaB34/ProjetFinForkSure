ae4147f519b9e0bdb9f41218b8a87ca1f8a95ca5
carla <carlita.b34@gmail.com>
2020-04-28 16:13:24 +0200

    master a jour

--- ForkProjetFinAnne/Assets/script/Health.cs
+++ ForkProjetFinAnne/Assets/script/Health.cs
@@ -5,21 +5,29 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     public int playerhealth = 2;
+    private int enemyhealth = 2;
+    public GameObject enemy;
+    private static bool isDash;
 
     public void Update() // la vie est cap a 2hp
     {
-        if(playerhealth > 2)
-    {
+        if (playerhealth > 2)
+        {
             playerhealth = 2;
         }
+        if (enemyhealth > 2)
+        {
+            enemyhealth = 2;
+        }
+        isDash = PlayerController.dashing;
     }
     void OnCollisionEnter(Collision collision) // on perd un point de vie si on touche un pic
     {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("pike"))
+        if (collision.gameObject.layer == LayerMask.NameToLayer("pike") && isDash == false)
         {
             Debug.Log("vous prenez 1 degat");
             playerhealth -= 1;
-            
+
         }
         if (collision.gameObject.layer == LayerMask.NameToLayer("heal")) // on gagne un point de vie quand on rammasse un heal
         {
@@ -30,7 +38,27 @@ public class Health : MonoBehaviour
         {
             Debug.Log("vous etes mort");
             Destroy(gameObject);
-            
+
         }
+
+        //enemy
+        if (collision.gameObject.layer == LayerMask.NameToLayer("pike") && isDash == true)
+        {
+
+            Debug.Log("l'enemy = 1 degat");
+            enemyhealth -= 1;
+            attackDash();
+
+        }
+    }
+
+    private void attackDash()
+    {
+        if (enemyhealth <= 0)
+        {
+            Debug.Log("enemy death");
+            Destroy(enemy);
+        }
+
     }
 }
