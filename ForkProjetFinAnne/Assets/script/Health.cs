3820bb4ad8a8af07c82b276c108fe8a8a3cee742
carla <carlita.b34@gmail.com>
2020-05-06 20:10:43 +0200

    commit testt

--- ForkProjetFinAnne/Assets/script/Health.cs
+++ ForkProjetFinAnne/Assets/script/Health.cs
@@ -7,8 +7,11 @@ using UnityEngine.SceneManagement;
 public class Health : MonoBehaviour
 {
     public int playerhealth = 2;
-    private int enemyhealth = 2;
+    public int enemyhealth = 2;
+    public int bosshealth = 5;
+
     public GameObject enemy;
+    public GameObject bossObjet;
     private static bool isDash;
 
     public void Update() // la vie est cap a 2hp
@@ -17,41 +20,65 @@ public class Health : MonoBehaviour
         {
             playerhealth = 2;
         }
+
         if (enemyhealth > 2)
         {
             enemyhealth = 2;
         }
+
+        if (bosshealth > 5)
+        {
+            bosshealth = 5;
+        }
+
+
         isDash = PlayerController.dashing;
     }
-    void OnCollisionEnter(Collision collision) // on perd un point de vie si on touche un pic
+    void OnCollisionEnter(Collision collision) 
     {
+        // on perd un point de vie si on touche un pic
         if (collision.gameObject.layer == LayerMask.NameToLayer("pike") && isDash == false)
         {
             Debug.Log("vous prenez 1 degat");
             playerhealth -= 1;
-
         }
-        if (collision.gameObject.layer == LayerMask.NameToLayer("heal")) // on gagne un point de vie quand on rammasse un heal
+
+        if (collision.gameObject.layer == LayerMask.NameToLayer("boss") && isDash == false)
+        {
+            Debug.Log("trouver");
+            Debug.Log("vous prenez 1 degat");
+            playerhealth -= 1;
+        }
+
+        // on gagne un point de vie quand on rammasse un heal
+        if (collision.gameObject.layer == LayerMask.NameToLayer("heal")) 
         {
             Debug.Log("vous avez recupéré un point de vie");
             playerhealth += 1;
         }
-        if (playerhealth == 0) // si vie = 0 on meurt
+
+        // si vie = 0 on meurt
+        if (playerhealth == 0) 
         {
             Debug.Log("vous etes mort");
             Destroy(gameObject);
             SceneManager.LoadScene("StartMenu");
-
         }
 
         //enemy
         if (collision.gameObject.layer == LayerMask.NameToLayer("pike") && isDash == true)
         {
-
             Debug.Log("l'enemy = 1 degat");
             enemyhealth -= 1;
             attackDash();
+        }
 
+        if (collision.gameObject.layer == LayerMask.NameToLayer("boss") && isDash == true)
+        {
+            Debug.Log("toucher");
+            Debug.Log("l'enemy = 1 degat");
+            bosshealth -= 1;
+            attackDash();
         }
     }
 
@@ -63,5 +90,10 @@ public class Health : MonoBehaviour
             Destroy(enemy);
         }
 
+        if (bosshealth <= 0)
+        {
+            Debug.Log("enemy death");
+            Destroy(bossObjet);
+        }
     }
 }
