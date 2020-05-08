75f88b1c512170c01fa8baba6fcd59f9fba26032
carla <carlita.b34@gmail.com>
2020-05-08 20:41:05 +0200

    Revert "Merge branch 'master' of https://github.com/CarlaB34/ProjetFinForkSure"
    This reverts commit 882f32bb853f689c09e785b7135ce41eef8317f6, reversing
    changes made to 2668f1eacf645ae8b68a90c582a7190aeb677ed9.

---
+++ ForkProjetFinAnne/Assets/script/Health.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Health : MonoBehaviour
+{
+    public int playerhealth = 2;
+    private int enemyhealth = 2;
+    public GameObject enemy;
+    private static bool isDash;
+
+    public void Update() // la vie est cap a 2hp
+    {
+        if (playerhealth > 2)
+        {
+            playerhealth = 2;
+        }
+        if (enemyhealth > 2)
+        {
+            enemyhealth = 2;
+        }
+        isDash = PlayerController.dashing;
+    }
+    void OnCollisionEnter(Collision collision) // on perd un point de vie si on touche un pic
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("pike") && isDash == false)
+        {
+            Debug.Log("vous prenez 1 degat");
+            playerhealth -= 1;
+
+        }
+        if (collision.gameObject.layer == LayerMask.NameToLayer("heal")) // on gagne un point de vie quand on rammasse un heal
+        {
+            Debug.Log("vous avez recupéré un point de vie");
+            playerhealth += 1;
+        }
+        if (playerhealth == 0) // si vie = 0 on meurt
+        {
+            Debug.Log("vous etes mort");
+            Destroy(gameObject);
+            SceneManager.LoadScene("StartMenu");
+
+        }
+
+        //enemy
+        if (collision.gameObject.layer == LayerMask.NameToLayer("pike") && isDash == true)
+        {
+
+            Debug.Log("l'enemy = 1 degat");
+            enemyhealth -= 1;
+            attackDash();
+
+        }
+    }
+
+    private void attackDash()
+    {
+        if (enemyhealth <= 0)
+        {
+            Debug.Log("enemy death");
+            Destroy(enemy);
+        }
+
+    }
+}
---
+++ ForkProjetFinAnne/Assets/script/PlayerScript/Heal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heal : MonoBehaviour
+{
+    
+    void OnCollisionEnter(Collision collision) // fait disparaitre le heal quand on le ramasse
+    {
+
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
