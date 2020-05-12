56b66ccd0dbfc6f4e6a93b071fc120ffd3535796
carla <carlita.b34@gmail.com>
2020-05-12 16:13:23 +0200

    boss

--- ForkProjetFinAnne/Assets/script/IA/Enemi.cs
+++ ForkProjetFinAnne/Assets/script/IA/Enemi.cs
@@ -67,7 +67,8 @@ public class Enemi : MonoBehaviour
      }*/
     #endregion
 
-    public AIPath aIPath;
+    #region tentative 3 Pathfinding//enemi
+    /*public AIPath aIPath;
 
     void Update()
     {
@@ -79,5 +80,77 @@ public class Enemi : MonoBehaviour
         {
             transform.localScale = new Vector3(1f, 1f, 1f);
         }
+    }*/
+    #endregion
+
+    #region tentative 4//enemi(3)
+
+    public int maxHealth = 40;
+
+    public float speed;
+    public float attackRange;
+    public float attackDelay;
+    public float chaseRange;
+    public float bulletForce;
+    private float lastAttackTime;
+    private float distanceToTarget;
+
+    public Transform target;
+    public GameObject projectile;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
     }
+
+    private void FixedUpdate()
+    {
+        if (distanceToTarget < chaseRange)
+        {
+            Chase(target.position);
+        }
+        else
+        {
+            rb.velocity = Vector3.zero;
+        }
+    }
+
+    private void Update()
+    {
+        distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+        if (distanceToTarget < attackRange)
+        {
+            if (Time.time > lastAttackTime + attackDelay)
+            {
+                RaycastHit2D Hit = Physics2D.Raycast(transform.position, transform.up, attackRange, 1 << LayerMask.NameToLayer("Player"));
+                if (Hit)
+                {
+                    Fire();
+                    lastAttackTime = Time.time;
+                }
+            }
+        }
+    }
+
+    private void Chase(Vector3 target)
+    {
+        Vector3 targetDir = target - transform.position;
+        float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg - 90f;
+
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 180);
+
+        rb.velocity = targetDir.normalized * speed;
+    }
+
+    private void Fire()
+    {
+        GameObject newBullet = Instantiate(projectile, transform.position, transform.rotation);
+        newBullet.GetComponent<Rigidbody>().AddForce(transform.up * bulletForce, ForceMode.Impulse);
+    }
+
+
+    #endregion
 }
