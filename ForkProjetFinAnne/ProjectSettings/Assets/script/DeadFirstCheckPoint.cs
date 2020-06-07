654942e543fa4f15f4112f8030fcd2043b92fb37
carla <carlita.b34@gmail.com>
2020-06-07 17:47:17 +0200

    TOUT BON

---
+++ ForkProjetFinAnne/ProjectSettings/Assets/script/DeadFirstCheckPoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadFirstCheckPoint : MonoBehaviour
+{
+    public Vector3 respawnPosition;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "player")
+        {
+            col.transform.position = respawnPosition;
+        }
+    }
+}
